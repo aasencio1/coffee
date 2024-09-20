2717993655fe95b3f3792b0548e06f85d62b125a
Jose Asencio <asdrubal.asencio@gmail.com>
2024-09-20 16:26:42 -0400

    It has been added ORM with only Coffee Entity

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,52 +1,81 @@
 import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
+import { Repository } from 'typeorm';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 
 @Injectable()
 export class CoffeesService {
- private coffees = [
+
+  constructor(
+     @InjectRepository(Coffee)
+     private readonly coffeeRepository: Repository <Coffee>,
+  ){}
+
+/*-private coffees = [
    {
      id:1,
      name: 'Shipwreck Roast',
      brand: 'Buddy Brew',
      flavors: ['chocolate','vanilla'],
    },
- ];
+ ];*/
 
  findAll(){
-    return this.coffees;
+   // return this.coffees;
+   return this.coffeeRepository.find();
  }
 
- findOne (id: string){
+ async findOne (id: string){
     // throw 'A ramdom Error';
-    const coffee = this.coffees.find(item=> item.id ===+id)
+   // const coffee = this.coffees.find(item=> item.id ===+id)
+   //await para funciones asincronas y await se usa para esperar funciones marcadas como asincronas
+   const coffee = await this.coffeeRepository.findOne({ where: { id : +id  }});
     if (!coffee){ //manejo de errores
         //throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
         throw new NotFoundException (`Coffee #${id} not found`)
     }
     return coffee;
  }
- create(createCoffeeDto: any)   // no se valida el tipo de datos, es decir puede ser cualquier tipo de datos
+ create(createCoffeeDto: CreateCoffeeDto)   // any: no se valida el tipo de datos, es decir puede ser cualquier tipo de datos
  {
-  this.coffees.push(createCoffeeDto);
-  return createCoffeeDto; // if it is return enable, you can see the response json. If do not, the response is "no body returned for response"
+  // this.coffees.push(createCoffeeDto);
+  const coffee = this.coffeeRepository.create(createCoffeeDto)
+  return this.coffeeRepository.save(coffee);
+  //return createCoffeeDto; // if it is return enable, you can see the response json. If do not, the response is "no body returned for response"
  } 
 
- update(id: string, updateCoffeeDto: any)
+ async update(id: string, updateCoffeeDto: UpdateCoffeeDto)   //antes any ...
  {
-    const existingCoffee = this.findOne(id);
+   /* const existingCoffee = this.findOne(id);
     if (existingCoffee)
         {
             // updating the existing entity
         }
+    */ 
+    const coffee = await this.coffeeRepository.preload ({
+      id: +id,
+      ...updateCoffeeDto   //copia todo lo que tiene del updateCoffeeDto en la carga del objetotraido de DB 
+    }) 
+    if (!coffee) {
+       throw new NotFoundException(`Coffee #${id} not found`);
+    }  
+    return this.coffeeRepository.save(coffee)         
  }
 
- remove (id: string)
+ async remove (id: string)
  {
-    const coffeeIndex = this.coffees.findIndex(item => item.id === +id)   // comprador estricto de igualdad 
+   const coffee = await this.findOne(id);
+   return this.coffeeRepository.remove(coffee);
+
+   /* const coffeeIndex = this.coffees.findIndex(item => item.id === +id)   // comprador estricto de igualdad 
     if (coffeeIndex >=0)
         {
             this.coffees.splice(coffeeIndex, 1);  // splice para modificar el contenido de un arreglo, es decir, al coincidir el valor del indice, se elimina 1 elemento, es decir el
             // elemento actual del arreglo
         }
+    */
  } 
 
 
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()  // sql table by default gets ths same name of the entity class  , un nombre diferente, inside @Entity decorator (ej. @Entity('Test'))
+
 export class Coffee 
 {
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+
+    @Column('json', {  nullable: true} )  // due is an array
     flavors: string[];
 }
