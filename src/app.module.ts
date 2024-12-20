065734d83568ae2ae223982ef18416ec4addb5d7
Jose Asencio <asdrubal.asencio@gmail.com>
2024-12-20 19:27:59 -0400

    add class diagram which represent main concepts for no sql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 //import { CoffeesController } from './coffees/coffees.controller';
 //import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+  ],
   controllers: [AppController],  // Se ELIMINA el CoffeesController del Modulo prinicipal y se define dentro del Modulo a usar.
   providers: [AppService], // Se ELIMINA el CoffeesService del Modulo prinicipal y se define dentro del Modulo a usar.
 })
