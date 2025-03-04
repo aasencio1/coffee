42109c909c7bc7f9938a48842fc37f3b7e041293
aasencio1 <asdrubal.asencio@gmail.com>
2025-03-04 18:17:34 -0400

    solved problem with updateflavourDTO

--- src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
@@ -5,7 +5,7 @@ import { CreateCoffeeDto } from "../create-coffee.dto/create-coffee.dto";
 export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto){   // Importa la clase de la cual heredara el comportamiento ; inherit all the validation
     //aplies to the validation on CreateCoffeeDto
     // use question mark after the name of the property means optional. 
-   /* readonly name?: string;
+readonly name?: string;
     readonly  brand?: string;
-    readonly flavors?: string[];*/
+    readonly flavors?: string[];
 }
